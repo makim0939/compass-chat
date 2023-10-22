54417725183cb0b47dd6c5ca8a6d3b4149798b11
makim0939 <makim0939@gmail.com>
2023-10-22 14:27:11 +0900

    ユーザIDからトークルームとユーザのも付けを取得するAPIを作成

---
+++ src/app/api/room_user/user_id/[user_id]/route.ts
@@ -0,0 +1,14 @@
+import supabase from "@/app/utils/supabase";
+import { NextResponse } from "next/server";
+
+//ユーザと紐づけられたトークルームの取得
+export const GET = async (req: Request) => {
+  const user_id = req.url.split("user_id/")[1];
+  try {
+    const { data, error } = await supabase.from("room_user").select("*").eq("user_id", user_id);
+    if (error) throw error;
+    return NextResponse.json({ message: "Success", data }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+};
