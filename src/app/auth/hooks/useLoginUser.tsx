10219679779b252a41dc580c849892a05b9c31ec
makim0939 <makim0939@gmail.com>
2023-10-22 10:52:57 +0900

    セッション、ユーザを取得しAtomで管理

--- src/app/auth/hooks/useLoginUser.tsx
+++ src/app/auth/hooks/useLoginUser.tsx
@@ -1,23 +1,27 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import { Session } from "@supabase/supabase-js";
 import { Profile } from "@/app/types/database.types";
+import { fetchData } from "@/app/utils/clientFunctions";
+import { set } from "react-hook-form";
 
 const getUser = async (id: string): Promise<Profile> => {
   console.log("fetch loginUser");
-  const url = document.location.origin + "/api/user/id/" + id;
+  const url = document.location.origin + "/api/profile/id/" + id;
   const loginUser = await fetchData<Profile>(url);
   return loginUser;
 };
 
-const useLoginUser = ({ session }: { session: Session | undefined }): UseQueryResult<Profile> =>
+const useLoginUser = (session: Session | undefined): UseQueryResult<Profile> =>
   useQuery({
-    queryKey: ["loginUser", session!.user.id],
+    queryKey: ["loginUser", session?.user?.id],
     queryFn: async () => {
-      getUser(session!.user.id);
+      const loginUser = await getUser(session!.user.id);
+      return loginUser;
     },
     staleTime: Infinity,
-    enabled: !!session?.user?.id,
+    enabled: !!session,
   });
 
 export default useLoginUser;
