d8cc64c5e7a27018be25f81a35b7d485655833b1
makim0939 <makim0939@gmail.com>
2023-10-23 08:43:59 +0900

    コネクション型の定義を追加

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -2,7 +2,8 @@ import { Database } from "./database.types";
 
 export type Profile = Database["public"]["Tables"]["profile"]["Row"];
 export type ProfileInsertProps = Database["public"]["Tables"]["profile"]["Insert"];
-export type RoomUser = Database["public"]["Tables"]["room_user"]["Row"];
+export type RoomUserRelation = Database["public"]["Tables"]["room_user"]["Row"];
 export type MessageData = Database["public"]["Tables"]["message"]["Row"];
 export type Room = Database["public"]["Tables"]["room"]["Row"];
 export type GeolocationData = Database["public"]["Tables"]["geolocation"]["Row"];
+export type Connection = Database["public"]["Tables"]["connection"]["Row"];
